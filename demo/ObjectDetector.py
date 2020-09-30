4e028db964234670e6fe182a28abef10933a94e2
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-09-30 20:38:54 +0200

    updated inference model path

--- demo/ObjectDetector.py
+++ demo/ObjectDetector.py
@@ -44,12 +44,13 @@ class Detector:
 
 		# get Model
 		try:
-			model_dir = os.path.abspath(os.environ.get('PS_MODEL_PATH', ''))
+			model_dir = os.path.abspath(os.environ.get('PS_MODEL_PATH', '/artifacts/models'))
 			model_path = os.path.join(model_dir, '/detectron/model_final.pth')
 			if os.path.isfile(model_path):
 				print('Using Trained Model {}'.format(model_path), flush=True)
 				self.cfg.MODEL.WEIGHTS = model_path
 			else:
+				print('No Model Found at {}'.format(model_path), flush=True)
 				model_path = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/"+self.model)
 				self.cfg.MODEL.WEIGHTS = model_path
 		except Exception as e:
