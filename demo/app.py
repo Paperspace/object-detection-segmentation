e6f4092faec704aa9399e9ce282d08257a3b0775
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-09-30 19:45:57 +0200

    updated code errors

--- demo/app.py
+++ demo/app.py
@@ -5,6 +5,7 @@ from PIL import Image
 import requests
 import os
 import img_transforms
+import logging
 
 app = Flask(__name__)
 detector = Detector()
@@ -14,116 +15,120 @@ RENDER_FACTOR = 35
 
 # function to load img from url
 def load_image_url(url):
-	response = requests.get(url)
-	img = Image.open(io.BytesIO(response.content))
-	return img
+    response = requests.get(url)
+    img = Image.open(io.BytesIO(response.content))
+    return img
+
 
 # run inference using image transform to reduce memory
-def run_inference_transform(img_path = 'file.jpg', transformed_path = 'file_transformed.jpg'):
+def run_inference_transform(img_path='file.jpg', transformed_path='file_transformed.jpg'):
+    # get height, width of image
+    original_img = Image.open(img_path)
 
-	# get height, width of image
-	original_img = Image.open(img_path)
+    # transform to square, using render factor
+    transformed_img = img_transforms._scale_to_square(original_img, targ=RENDER_FACTOR * 16)
+    transformed_img.save(transformed_path)
 
-	# transform to square, using render factor
-	transformed_img = img_transforms._scale_to_square(original_img, targ=RENDER_FACTOR*16)
-	transformed_img.save(transformed_path)
+    # run inference using detectron2
+    untransformed_result = detector.inference(transformed_path)
 
-	# run inference using detectron2
-	untransformed_result = detector.inference(transformed_path)
+    # unsquare
+    result_img = img_transforms._unsquare(untransformed_result, original_img)
 
-	# unsquare
-	result_img = img_transforms._unsquare(untransformed_result, original_img)
+    # clean up
+    try:
+        os.remove(img_path)
+        os.remove(transformed_path)
+    except:
+        pass
 
-	# clean up
-	try:
-		os.remove(img_path)
-		os.remove(transformed_path)
-	except:
-		pass
+    return result_img
 
-	return result_img
 
 # run inference using detectron2
-def run_inference(img_path = 'file.jpg'):
+def run_inference(img_path='file.jpg'):
+    # run inference using detectron2
 
-	# run inference using detectron2
-	result_img = detector.inference(img_path)
+    try:
+        result_img = detector.inference(img_path)
+    except Exception as e:
+        app.logger.critical(str(e))
+    # clean up
+    try:
+        os.remove(img_path)
+    except:
+        pass
 
-	# clean up
-	try:
-		os.remove(img_path)
-	except:
-		pass
-
-	return result_img
+    return result_img
 
 
 @app.route("/")
 def index():
-
-
-	url = request.url
-	model_id = os.environ.get('PAPERSPACE_METRIC_WORKLOAD_ID', 'model_id')
-	return render_template('index.html', url=url,model_id=model_id)
+    url = request.url
+    model_id = os.environ.get('PAPERSPACE_METRIC_WORKLOAD_ID', 'model_id')
+    return render_template('index.html', url=url, model_id=model_id)
 
 
 @app.route("/detect", methods=['POST', 'GET'])
 def upload():
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		try:
+        try:
 
-			# open image
-			file = Image.open(request.files['file'].stream)
+            # open image
+            file = Image.open(request.files['file'].stream)
 
-			# remove alpha channel
-			rgb_im = file.convert('RGB')
-			rgb_im.save('file.jpg')
-		
-		# failure
-		except:
+            # remove alpha channel
+            rgb_im = file.convert('RGB')
+            rgb_im.save('file.jpg')
 
-			return render_template("failure.html")
+        # failure
+        except:
 
-	elif request.method == 'GET':
+            return render_template("failure.html")
 
-		# get url
-		url = request.args.get("url")
+    elif request.method == 'GET':
 
-		# save
-		try:
-			# save image as jpg
-			# urllib.request.urlretrieve(url, 'file.jpg')
-			rgb_im = load_image_url(url)
-			rgb_im = rgb_im.convert('RGB')
-			rgb_im.save('file.jpg')
+        # get url
+        url = request.args.get("url")
 
-		# failure
-		except:
-			return render_template("failure.html")
+        # save
+        try:
+            # save image as jpg
+            # urllib.request.urlretrieve(url, 'file.jpg')
+            rgb_im = load_image_url(url)
+            rgb_im = rgb_im.convert('RGB')
+            rgb_im.save('file.jpg')
 
-	# run inference
-	# result_img = run_inference_transform()
-	result_img = run_inference('file.jpg')
+        # failure
+        except:
+            return render_template("failure.html")
 
-	# create file-object in memory
-	file_object = io.BytesIO()
+    # run inference
+    # result_img = run_inference_transform()
+    try:
+        result_img = run_inference('file.jpg')
 
-	# write PNG in file-object
-	result_img.save(file_object, 'PNG')
+        # create file-object in memory
+        file_object = io.BytesIO()
 
-	# move to beginning of file so `send_file()` it will read from start    
-	file_object.seek(0)
+        # write PNG in file-object
+        result_img.save(file_object, 'PNG')
 
-	return send_file(file_object, mimetype='image/jpeg')
+        # move to beginning of file so `send_file()` it will read from start
+        file_object.seek(0)
+    except Exception as e:
+        app.logger.critical(str(e))
+        return render_template("failure.html")
+
+    return send_file(file_object, mimetype='image/jpeg')
 
 
 if __name__ == "__main__":
+    # get port. Default to 8080
+    port = int(os.environ.get('PORT', 8080))
 
-	# get port. Default to 8080
-	port = int(os.environ.get('PORT', 8080))
+    logging.basicConfig(level=logging.DEBUG)
 
-	# run app
-	app.run(host='0.0.0.0', port=port, debug=True)
-
-
+    # run app
+    app.run(host='0.0.0.0', port=port, debug=True)
